5227b020e21b80bd10f28b63c895591e0da392a7
avraham yossef <avrahamyossef3@gmail.com>
2021-08-10 13:11:57 +0300

    change project name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={"/ReactProject"}>
+    <Router basename={"/books-store"}>
       <App />
     </Router>
   </React.StrictMode>,
